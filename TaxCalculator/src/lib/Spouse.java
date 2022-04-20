7d2d17341543b9e7d8acfca9360a2f4af91259e2
aaldishantuy <55171521+aaldishantuy@users.noreply.github.com>
2022-04-21 00:06:47 +0700

    Update Spouse 1

--- TaxCalculator/src/lib/Spouse.java
+++ TaxCalculator/src/lib/Spouse.java
@@ -1,1 +1,14 @@
+public class Spouse extends Employee {
 
+	  private String spouseName;
+	  private String spouseIdNumber;
+
+    public Spouse () {
+        super ();
+    }
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+		this.spouseName = spouseName;
+		this.spouseIdNumber = idNumber;
+	}
+} 
