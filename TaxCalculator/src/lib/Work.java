4178e5a2f67a6b70e79aebc6db35ad426a86400a
aaldishantuy <55171521+aaldishantuy@users.noreply.github.com>
2022-04-21 00:07:28 +0700

    Update Work1

--- TaxCalculator/src/lib/Work.java
+++ TaxCalculator/src/lib/Work.java
@@ -1,1 +1,14 @@
+package lib;
 
+import java.time.LocalDate;
+
+public class Work extends Employee{
+
+	private int yearJoined;
+	private int monthJoined;
+	private int dayJoined;
+	private int monthWorkingInYear;
+
+    public Work (){
+          super();
+    }
